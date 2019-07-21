759ee26fd1a4dd63870f10e257615c4cd378d8ed
Ashna Shah-Grover <ashnashahgrover777@gmail.com>
2019-07-21 16:59:40 -0400

    Done.

--- app/models/startup.rb
+++ app/models/startup.rb
@@ -1,3 +1,55 @@
+require 'pry'
 class Startup
+  attr_reader :name, :founder, :domain
+  @@all = []
+
+  def initialize(name, founder, domain)
+    @name = name
+    @founder = founder
+    @domain = domain
+    @@all << self
+  end
+
+  def pivot(domain, name)
+    @domain = domain
+    @name = name
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.find_by_founder(founder_name)
+    self.all.find {|startup| startup.founder == founder_name}
+  end
+
+  def self.domains
+    self.all.map {|startup| startup.domain}
+  end
+
+  def sign_contract(capitalist, type, amount)
+    FundingRound.new(self, capitalist, type, amount)
+  end
+
+  def rounds
+    FundingRound.all.select {|round| round.startup == self}
+  end
+
+  def num_funding_rounds
+    self.rounds.size
+  end
+
+  def total_funds
+    self.rounds.map {|round| round.investment}.sum
+  end
+
+  def investors
+    self.rounds.map {|round| round.venture_capitalist}.uniq
+  end
+
+  def big_investors
+    self.investors.select {|investor| VentureCapitalist.tres_commas_club.include?(investor)}
+  end
+
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -6,6 +6,27 @@ end
 # Insert code here to run before hitting the binding.pry
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
+applebright = Startup.new("AppleBright", "Ashna", "Zen")
+diamondDeal = Startup.new("DiamondDeal", "Manish", "Diamonds")
+eccentricSoul = Startup.new("EccentricSoul", "Leo", "Music")
+fzi = Startup.new("FZI", "SokeiAn", "Zen")
+
+isa = VentureCapitalist.new("Isabel", 2000000000)
+reidar = VentureCapitalist.new("Reidar", 7000000000)
+allie = VentureCapitalist.new("Allie", 1000000000)
+willis = VentureCapitalist.new("Willis", 400)
+
+applebright.sign_contract(isa, "A", 3500)
+applebright.sign_contract(isa, "A", 3500)
+diamondDeal.sign_contract(isa, "A", 3500)
+applebright.sign_contract(allie, "B", 5670)
+
+reidar.offer_contract(eccentricSoul, "B", 54638)
+willis.offer_contract(diamondDeal, "R", 3400)
+isa.offer_contract(eccentricSoul, "B", 34552)
+isa.offer_contract(fzi, "B", 350)
+
 
 binding.pry
+
 0 #leave this here to ensure binding.pry isn't the last line
