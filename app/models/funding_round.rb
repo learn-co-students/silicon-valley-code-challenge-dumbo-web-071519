48019ac790beb84326746cbd3965ade2156ad3df
Ross Perkel <rossperkel@gmail.com>
2019-07-23 07:57:34 -0400

    Done.

--- app/models/funding_round.rb
+++ app/models/funding_round.rb
@@ -1,3 +1,17 @@
 class FundingRound
+    @@all = []
+    attr_accessor :type, :investment
+    attr_reader :startup, :venture_capitalist
 
+    def initialize(startup, venture_capitalist, type, investment)
+        @startup = startup
+        @venture_capitalist = venture_capitalist
+        @type = type
+        @investment = investment.to_f
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
 end
--- app/models/startup.rb
+++ app/models/startup.rb
@@ -1,3 +1,51 @@
 class Startup
+    attr_accessor :name
+    attr_reader :founder, :domain
+    @@all = []
 
+    def initialize(name, founder, domain)
+        @name = name
+        @founder = founder
+        @domain = domain
+        @@all << self
+    end
+
+    def pivot(domain, name)
+        @domain = domain
+        @name = name
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.find_by_founder(name)
+        @@all.find{|company| company.founder == name}
+    end
+
+    def self.domains
+        @all.select{|startup| startup.domain}
+    end
+
+    def sign_contract(venture_capitalist, type, investment)
+        FundingRound.new(self, venture_capitalist, type, investment)
+    end
+
+    def num_funding_rounds
+        FundingRound.all.select{|contract| contract.startup == self}.size
+    end
+
+    def total_funds
+        FundingRound.all.select{|contract| contract.startup == self}.inject
+    end
+
+    def investors
+        FundingRound.all.select{|contract| contract.venture_capitalist}.uniq
+    end
+
+    def big_investors
+        FundingRound.all.select{|contract| contract.venture_capitalist}.uniq && FundingRound.all.select{|contract| contract.venture_capitalist.tres_comma_club == true}
+    end
+    
+    
 end
